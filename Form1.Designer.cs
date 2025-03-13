ae8d04c11f6179d0cb278020eb631dd634d2f2c1
PullasorsaHUB <rasmus.puromaki@hotmail.fi>
2025-03-13 11:33:38 +0200

    Nimi vaihdettu sovellukseen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1552, 859);
+            Name = "Form1";
+            Text = "Appointment";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,15 @@ namespace Appointment
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

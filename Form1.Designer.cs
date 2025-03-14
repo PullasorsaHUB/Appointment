926562e31c194b579221c4fddd22ffc655f9fa4e
PullasorsaHUB <rasmus.puromaki@hotmail.fi>
2025-03-14 13:07:06 +0200

    Lisättiin kalenteri ja vaihdettin nimet checkedboxeissa

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            monthCalendar1 = new MonthCalendar();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(264, 491);
+            button1.Name = "button1";
+            button1.Size = new Size(156, 51);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(466, 491);
+            button2.Name = "button2";
+            button2.Size = new Size(156, 51);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(301, 185);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 3;
+            monthCalendar1.DateChanged += monthCalendar1_DateChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(738, 185);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(105, 25);
+            checkBox1.TabIndex = 4;
+            checkBox1.Text = "Parturi";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(738, 254);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(105, 25);
+            checkBox2.TabIndex = 5;
+            checkBox2.Text = "Hieronta";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(738, 332);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(105, 25);
+            checkBox3.TabIndex = 6;
+            checkBox3.Text = "Solarium";
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
             ClientSize = new Size(1552, 859);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(monthCalendar1);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Appointment";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private MonthCalendar monthCalendar1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,35 @@ namespace Appointment
         {
 
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }

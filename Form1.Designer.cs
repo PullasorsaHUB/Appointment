962e232aa73271f4e6f50440e9f23caad83b17ef
Rasmus Puromäki <rasmus.puromaki@hotmail.fi>
2025-03-21 12:57:51 +0200

    Alusta about

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            monthCalendar1 = new MonthCalendar();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(93, 94);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 0;
+            monthCalendar1.DateChanged += monthCalendar1_DateChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Parturi", "Hieronta", "Solarium" });
+            comboBox1.Location = new Point(93, 297);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(199, 23);
+            comboBox1.TabIndex = 1;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(553, 94);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 40);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
@@ -36,6 +66,9 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(64, 64, 64);
             ClientSize = new Size(1207, 614);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(monthCalendar1);
             Margin = new Padding(2);
             Name = "Form1";
             Text = "Appointment";
@@ -44,5 +77,9 @@
         }
 
         #endregion
+
+        private MonthCalendar monthCalendar1;
+        private ComboBox comboBox1;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,83 +6,10 @@ namespace Appointment
         public Form1()
         {
             InitializeComponent();
-            InitUI();
-        }
-
-
-        private void InitUI()
-        {
-            this.Text = "Ajanvaraus";
-            this.Width = 400;
-            this.Height = 400;
-
-            MonthCalendar kalenteri = new MonthCalendar { Left = 10, Top = 10 };
-            this.Controls.Add(kalenteri);
-
-            Label palveluLabel = new Label { Text = "Valitse palvelu:", Left = 10, Top = 190, Width = 120 };
-            this.Controls.Add(palveluLabel);
-
-            ComboBox palveluComboBox = new ComboBox { Left = 10, Top = 210, Width = 150 };
-            palveluComboBox.Items.AddRange(new string[] { "Parturi", "Hieronta", "Solarium" });
-            this.Controls.Add(palveluComboBox);
-
-            Label aikaLabel = new Label { Text = "Valitse aika:", Left = 10, Top = 240, Width = 120 };
-            this.Controls.Add(aikaLabel);
-
-            ListBox aikaListBox = new ListBox { Left = 10, Top = 260, Width = 150, Height = 80 };
-            aikaListBox.Items.AddRange(new string[] { "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00" ,"16:00", "17:00" });
-            this.Controls.Add(aikaListBox);
-
-            Button varaaButton = new Button { Text = "Varaa", Left = 10, Top = 350, Width = 150 };
-            this.Controls.Add(varaaButton);
-
-            ListView varausLista = new ListView { Left = 180, Top = 210, Width = 200, Height = 150, View = View.List };
-            this.Controls.Add(varausLista);
-
-            varaaButton.Click += (sender, e) =>
-            {
-                if (palveluComboBox.SelectedItem == null || aikaListBox.SelectedItem == null)
-                {
-                    MessageBox.Show("Valitse palvelu ja aika.");
-                    return;
-                }
-
-                string varaus = $"{kalenteri.SelectionStart.ToShortDateString()} - {aikaListBox.SelectedItem} - {palveluComboBox.SelectedItem}";
-                if (!varatutAjat.Contains(varaus))
-                {
-                    varatutAjat.Add(varaus);
-                    varausLista.Items.Add(varaus);
-                    MessageBox.Show("Aika varattu onnistuneesti!");
-                }
-                else
-                {
-                    MessageBox.Show("Aika on jo varattu.");
-                }
-            };
         }
 
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox3_CheckedChanged(object sender, EventArgs e)
         {
 
         }
@@ -93,11 +20,6 @@ namespace Appointment
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click_1(object sender, EventArgs e)
         {
 
         }

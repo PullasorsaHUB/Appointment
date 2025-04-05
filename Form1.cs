52ddf296a177635863e3815a742681747295ccea
PullasorsaHUB <rasmus.puromaki@hotmail.fi>
2025-04-05 14:40:52 +0300

    Ajan varaus melkein valmis pientä lisäystä tarvitsee esim. ei voida varata hierontaa ja parturi palvelua samalle ajalle ja tämän jälkeen poistaisi tämän ajan sieltä kellon ajoista

--- Form1.cs
+++ Form1.cs
@@ -2,37 +2,63 @@ namespace Appointment
 {
     public partial class Form1 : Form
     {
-        private Dictionary<DateTime,string> varatutAjat = new Dictionary<DateTime,string>();
+        private Dictionary<DateTime,AjanVaraukset> varatutAjat = new Dictionary<DateTime,AjanVaraukset>();
         public Form1()
         {
             
             InitializeComponent();
         }
-
-
         private void Form1_Load(object sender, EventArgs e)
         {
             
         }
 
+        public class AjanVaraukset
+        {
+            public string Time { get; set; }
+            public string Service { get; set; } // Default to massage
+
+            public override string ToString()
+            {
+                return $" Aika: {Time}, Palvelu: {Service}";
+            }
+        }
+
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
         {
-            
-            DateTime selectedDate = e.Start;
-            if(!varatutAjat.ContainsKey(selectedDate))
-            {
-                varatutAjat.Add(selectedDate, "Appintment for user");
-                MessageBox.Show($"Appointment added for {selectedDate.ToShortDateString()}");
-            }
-            else
-            {
-                MessageBox.Show($"Date {selectedDate.ToShortDateString()} is already booked");
-            }
+        
         }   
 
         private void button1_Click(object sender, EventArgs e)
         {
             // VARAA NAPPI
+            DateTime selectedDate = monthCalendar1.SelectionStart;
+
+            string selectedTime = comboBox2.SelectedItem?.ToString();
+            string selectedService = comboBox1.SelectedItem?.ToString();
+
+            if(string.IsNullOrEmpty(selectedService) || string.IsNullOrEmpty(selectedTime))
+            {
+                MessageBox.Show("Please select both a service and a time.");
+                return;
+            }
+
+            DateTime appointmentDateTime = selectedDate.Date.Add(TimeSpan.Parse(selectedTime));
+
+            if(!varatutAjat.ContainsKey(appointmentDateTime))
+            {
+                varatutAjat[appointmentDateTime] = new AjanVaraukset
+                {
+                    Time = selectedTime,
+                    Service = selectedService
+                };
+
+                MessageBox.Show($"Appointment booked\n{appointmentDateTime}\nService: {selectedService}");
+            }
+            else
+            {
+                MessageBox.Show("This time slot is already booked!");
+            }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)

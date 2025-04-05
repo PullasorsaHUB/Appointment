f09352c2c667bb2bcd95d4b520ccbcdc4e33bbfa
PullasorsaHUB <rasmus.puromaki@hotmail.fi>
2025-04-05 13:48:47 +0300

    Poistin mun tekemän Home sivun en saanut sitä toimimaan

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace Appointment
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Home());
             Application.Run(new Form1());
         }
     }
